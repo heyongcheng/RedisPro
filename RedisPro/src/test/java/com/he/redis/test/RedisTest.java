35787065f001837a9286757cfdf85330f4860528
heyongcheng <178831731@qq.com>
2016-12-26 17:02:35 +0800

    事务支持

--- RedisPro/src/test/java/com/he/redis/test/RedisTest.java
+++ RedisPro/src/test/java/com/he/redis/test/RedisTest.java
@@ -3,7 +3,12 @@ package com.he.redis.test;
 import javax.annotation.Resource;
 
 import org.junit.Test;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.ValueOperations;
 
 import com.he.model.User;
@@ -37,8 +42,38 @@ public class RedisTest extends BaseJunit4Test{
 			System.out.println(user2);
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		
-		
+		}	
 	}
+	
+	@Test
+	public void testTransaction(){
+		
+		SessionCallback<User> callback = new SessionCallback<User>() {
+			
+			public <K, V> User execute(RedisOperations<K, V> operations) throws DataAccessException {
+				
+				ValueOperations<String, User> opsForValue = (ValueOperations<String, User>) operations.opsForValue();
+				User user = null;
+				do{
+					operations.watch((K) "明月");
+					user = opsForValue.get("明月");
+					if(user == null){
+						user = new User();
+						user.setName("明月");
+						user.setAge(12);
+					}
+					user.setAge(user.getAge() + 10);
+					operations.multi();
+					opsForValue.set("明月", user);
+				}while(operations.exec() == null);
+				
+				return user;
+			}
+		};
+		
+		User user = redisTemplate.execute(callback);
+		
+		System.out.println(user);
+	}
+	
 }
